3b567022bd356877c5d68767e7c7e84a2ceef37d
Md Sakibul Hossain <mdsakibulhossain420@gmail.com>
2026-05-02 10:56:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleContact() {
+  const details = document.getElementById('contactDetails');
+  const arrow = document.getElementById('arrow');
+  
+  details.classList.toggle('show');
+  arrow.classList.toggle('rotate');
+}
+function contact() {
+    alert("Please inbox us for service!");
+}
